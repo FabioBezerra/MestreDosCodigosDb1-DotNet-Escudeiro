ebf9c5ee4014d009bf1e87d4dac0d7e741bcbff8
Fabio Bezerra de Souza <fabio.bezerra@db1.com.br>
2021-01-18 19:30:50 -0400

    Criado novo projeto do tipo console

---
+++ Escudeiro.TrabalhandoNoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Escudeiro.TrabalhandoNoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
